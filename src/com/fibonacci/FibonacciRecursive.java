7ce370c60e7988cead74f176c5f47022b2af9a1b
Fernando Chaves <fernandolauc@gmail.com>
2025-08-15 12:27:50 -0300

    Att Fibonacci

--- src/com/fibonacci/FibonacciRecursive.java
+++ src/com/fibonacci/FibonacciRecursive.java
@@ -6,6 +6,7 @@ public class FibonacciRecursive {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Digite um número para saber seu Fibonacci: ");
         int num = sc.nextInt();
         while(num < 0){
